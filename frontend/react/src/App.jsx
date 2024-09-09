706f8b318e1b33afec79b7d45db51129c632eb99
mdshaheenmiah <mdshaheen60101@gmail.com>
2024-09-09 18:56:24 +0600

    update

--- frontend/react/src/App.jsx
+++ frontend/react/src/App.jsx
@@ -3,6 +3,8 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './portal/Home';
+import About from './portal/pages/About'
+import Service from './portal/pages/Service'
 
 
 function App() {
@@ -11,6 +13,9 @@ return (
   <Router>
     <Routes>
        <Route path="/" element={<Home />} />
+       <Route path="/About" element={<About />} />
+       <Route path="/Service" element={<Service />} />
+
     </Routes>
   </Router>
   )
